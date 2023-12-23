054b7a10b2e3f626555115fc7788964acb124ac9
AliShahSafdari <alishahsafdari905@gmail.com>
2023-12-23 22:02:44 +0430

    Practice the propes lesson

--- src/App.js
+++ src/App.js
@@ -2,24 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const classes = [
+    { name: "HTML" , finished : true},
+    { name : "JavaScript" , finished : false},
+    { name: 'CSS', finished: true},
+    { name: 'BootStrap', finished: false},
+    { name: "React", finished: true},
+    { name: "Python", finished: false},
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>{classes.map(( value, index) =>{
+      return value.finished && <Courses key={index} name={value.name} finished={ value.finished} ></Courses>
+
+    })}</div>
+
+  )
+}
+ 
+const Courses = (props) =>{
+  return(
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.finished}</p>
     </div>
   );
 }
+// const JObComponent = (props) =>{
+//   return(
+//    <div>
+//     <h1>${props.salary}</h1>
+//     <h2>{props.position}</h2>
+//     <p>{props.company}</p>
+//    </div> 
+//   );
+// }
+
+
 
 export default App;
