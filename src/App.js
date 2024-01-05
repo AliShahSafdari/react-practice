ee4b37e998860abbe7363394afd564c1f86b7f8d
AliShahSafdari <alishahsafdari905@gmail.com>
2024-01-05 21:49:14 +0430

    Add new button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import {useEffect, useState} from "react"
+import { useState} from "react"
 import './App.css';
 import Text from "./Text"; 
 
 function App() {
   const [showText, setShowText] = useState(false);
-  useEffect(()=>{
-    console.log("component did mount");
-  },[])
+  
   return(
    <div className="App">
     <button onClick={()=> setShowText(!showText)}> showText</button>
     {showText && <Text/>}
-  
+  <button>new </button>
    </div>
   )
 }
