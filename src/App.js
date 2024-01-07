f93ae0952c6c6e69099349406410ee9ef53101d0
AliShahSafdari <alishahsafdari905@gmail.com>
2024-01-07 09:03:41 +0430

    Fetch data

--- src/App.js
+++ src/App.js
@@ -3,42 +3,14 @@ import './App.css';
 import Course from "./Course";
 
 function App() {
-  const [courseList, setCourseList] = useState([]);
-  const [newCourse, setNewCourse] = useState("");
-  const handelChange = (event) =>{
-    setNewCourse(event.target.value);
-  }
-  const addCourse = ()=> {
-    const course = {
-      id: courseList.length === 0 ? 1 :courseList[courseList.length-1].id +1,
-      courseName : newCourse,
-      isCompleted : false
-    }
-    setCourseList([...courseList, course]);
-  }
-  const deleteCourse = (courseId)=>{
-   const newCourseList=  courseList.filter((course) => courseId !==course.id)
-   setCourseList(newCourseList);
-  }
-  const completedCourse = (courseId) =>{
-    const newCourseList = courseList.map((course) => {
-      if( course.id === courseId) return{...course, isCompleted: !course.isCompleted}
-      else return course
-    })
-   setCourseList(newCourseList);
-  }
+  fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data)=>{
+    console.log(data);
+  });
 
   return (
-    <div className='App'>
-      <div className='add-course'>
-        <input type='text' onChange={handelChange}></input>
-        <button onClick={addCourse}>Add Course</button>
-      </div>
-      <div className='list'>
-        {courseList.map((course, index)=>{
-          return (<Course key={index} course={course} completedCourse={completedCourse} deleteCourse={deleteCourse}></Course>)
-        })}
-      </div>
+    <div className="App">
+      <button>fetch data</button>
+      <p></p>
     </div>
   )
 }
