bc2cebedc99ca4bc19c50cc553fbe961ebd9ee96
AliShahSafdari <alishahsafdari905@gmail.com>
2024-01-07 20:51:09 +0430

    Installed axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import {useState} from "react"
 import './App.css';
 import Course from "./Course";
-
+import Axios from 'axios';
 function App() {
   fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data)=>{
     console.log(data);
   });
 
+  Axios.get('https://catfact.ninja/fact').then((res) => {
+    console.log(res);
+  })
+
   return (
     <div className="App">
-      <button>fetch data</button>
+      <button onClick={fetch}>fetch data</button>
       <p></p>
     </div>
   )
